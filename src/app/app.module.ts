643940337296bd202ceeedc43fd4f60548385181
ram071985 <reidmuchow@gmail.com>
2021-07-25 18:36:40 -0500

    Added HttpClient module, deleted views folder.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ChatAppComponent } from './chat-app.component';
-import { AuthComponent } from './views/auth/auth.component';
+import { AuthComponent } from './auth/auth.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ChatAppComponent, AuthComponent],
-  imports: [BrowserModule, NgbModule, FormsModule],
+  imports: [BrowserModule, NgbModule, FormsModule, HttpClientModule],
+  providers:[],
   bootstrap: [ChatAppComponent],
 })
 export class AppModule {}
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'auth-page',
-    template: 'auth.component.html'
+    templateUrl: './auth.component.html'
 })
 export class AuthComponent {
 
